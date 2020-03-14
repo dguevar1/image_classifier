a230f9773044ac49de09af83bd1498343ef06dc8
Dany Guevara <dany.guevara@pi-dan.com>
2020-03-13 21:02:35 -0400

    feat: Updated run_predict.bat.
    This commit updates run_predict.bat with additonal test cases.
    
    Resolves: #20

--- run_predict.bat
+++ run_predict.bat
@@ -1,2 +1,3 @@
-REM python predict.py flowers/train/1/image_06734.jpg checkpoints/vgg16_1_0.001_512_checkpoint.pth --gpu
-python predict.py flowers/train/1/image_06734.jpg checkpoints/vgg16_5_0.001_512_checkpoint.pth --gpu
+REM python predict.py flowers/train/1/image_06734.jpg checkpoints/vgg16_1_0.001_0.2_512_checkpoint.pth --gpu
+REM python predict.py flowers/train/1/image_06734.jpg checkpoints/vgg16_5_0.001_0.2_512_checkpoint.pth --gpu
+python predict.py flowers/train/1/image_06734.jpg checkpoints/vgg16_10_0.001_0.2_512_checkpoint.pth --gpu
